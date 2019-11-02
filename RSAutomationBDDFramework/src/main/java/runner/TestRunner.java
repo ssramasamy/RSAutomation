582aa0545f2880efbadb4348092dfcb4d1d32f09
User <User@DESKTOP-SFS223H.Home>
2019-11-02 12:46:33 +0000

    updated path

--- RSAutomationBDDFramework/src/main/java/runner/TestRunner.java
+++ RSAutomationBDDFramework/src/main/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:/Users/User/git/RSAutomationBDDFramework/src/main/java/Features/LoginAndVerify.feature",
+@CucumberOptions(features = "C:/Users/User/git/git/RSAutomation/RSAutomationBDDFramework/src/main/java/Features/LoginAndVerify.feature",
 				glue = {"stepDefinitions"},
 				plugin = {"pretty","html:test-output","json:json_output/cucumber.json","junit:xml_output/cucumber.xml"},
 				dryRun = false,
